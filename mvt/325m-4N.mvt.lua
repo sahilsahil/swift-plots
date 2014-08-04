c10b776087416f4472bcba6fab22810e394d58ec
Caglow <caglow@users.sourceforge.net>
2014-08-04 17:22:13 -0600

    Added miss vs. time for 325m asteroid and 2N and 4N thrust.

---
+++ mvt/325m-4N.mvt.lua
@@ -0,0 +1,16 @@
+-- *.mvt.lua
+-- inputs to generate graph of miss distance vs. laser active time
+
+inputs = {}
+
+-- use initial state vectors from separate file
+inputs.state = dofile("15yr.state.lua")
+
+-- asteroid diameter (m)
+inputs.ast_diam = 325
+
+-- force to use (N)
+inputs.force = 6
+
+-- list of times for laser to run (years)
+inputs.times = {2, 5, 7.5, 10, 12.5, 15, 17.5, 20, 25}
